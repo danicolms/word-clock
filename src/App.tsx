a8319abce4737de4abab9d238fc89939b47e21e7
danicolms <danicolms.c@gmail.com>
2022-05-20 16:14:45 +0200

    Updated utils v

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,7 @@ const defaultMatrix: IMatrix[] = [
 	{ letter: 'O', lit: false, position: [ 9, 10 ] }
 ];
 
-console.log('Running v1.0.4 and @danicolms/word-clock-utils v1.0.5');
+console.log('Running v1.0.5 with @danicolms/word-clock-utils v1.0.6');
 
 export const App: React.FunctionComponent = () => {
 	const [ matrix, setMatrix ] = useState<IMatrix[]>([ ...defaultMatrix ]);
